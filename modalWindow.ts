1efc93d4e63ec73d8cc22541363ece5a5e96ebd4
vvvportnov <vportnov.exceedteam@gmail.com>
2021-05-11 12:23:25 +0300

    Rewrite in typescript

--- modalWindow.ts
+++ modalWindow.ts
@@ -1,40 +1,63 @@
-"use strict";
-var Keys;
-(function (Keys) {
-    Keys["Esc"] = "Escape";
-    Keys["Ctrl"] = "Ctrl";
-})(Keys || (Keys = {}));
-var ModalWindow = /** @class */ (function () {
-    function ModalWindow() {
+enum Keys {
+    Esc = "Escape",
+    Ctrl = "Ctrl"
+}
+
+class ModalWindow {
+
+    private static instance : ModalWindow;
+    
+    private showed: boolean;
+    private modalWindow: HTMLElement;
+    private modalDialog: HTMLElement;
+    private modalContent: HTMLElement;
+    private modalHeader: HTMLElement;
+    private modalTitle: HTMLElement;
+    private modalClose: HTMLElement;
+    private modalBody: HTMLElement;
+    
+    private constructor() {
         this.initElements();
         this.initEvents();
         this.showed = false;
+
+
     }
-    ModalWindow.getInstance = function () {
+
+    public static getInstance(): ModalWindow {
         if (!ModalWindow.instance) {
             ModalWindow.instance = new ModalWindow();
         }
+
         return ModalWindow.instance;
-    };
-    ModalWindow.prototype.initElements = function () {
+    }
+
+    public initElements(): void {
         //Create elements
         this.modalWindow = document.createElement('div');
         this.modalWindow.setAttribute('class', 'modal');
+
         this.modalDialog = document.createElement('div');
         this.modalDialog.setAttribute('class', 'modal-dialog');
+
         this.modalContent = document.createElement('div');
         this.modalContent.setAttribute('class', 'modal-content');
+
         this.modalHeader = document.createElement('div');
         this.modalHeader.setAttribute('class', 'modal-header');
+        
         this.modalTitle = document.createElement('h3');
         this.modalTitle.setAttribute('class', 'modal-title');
+
         this.modalClose = document.createElement('a');
         this.modalClose.title = "Close";
         this.modalClose.innerHTML = "x";
         this.modalClose.setAttribute('href', '#close');
-        this.modalClose.setAttribute('class', 'modal-close');
+        this.modalClose.setAttribute('class', 'modal-close')
+
         this.modalBody = document.createElement('div');
         this.modalBody.setAttribute('class', 'modal-body');
+
         //Combine elements
         this.modalWindow.appendChild(this.modalDialog);
         this.modalDialog.appendChild(this.modalContent);
@@ -42,56 +65,65 @@ var ModalWindow = /** @class */ (function () {
         this.modalHeader.appendChild(this.modalTitle);
         this.modalHeader.appendChild(this.modalClose);
         this.modalContent.appendChild(this.modalBody);
-        var head = document.querySelector('head');
-        var body = document.querySelector('body');
-        var styleLink = document.createElement('link');
+
+        const head = document.querySelector('head');
+        const body = document.querySelector('body');
+        const styleLink = document.createElement('link');
         styleLink.rel = 'stylesheet';
         styleLink.href = 'modalWindow.css';
-        head.appendChild(styleLink);
+        
+        head.appendChild(styleLink)
         body.appendChild(this.modalWindow);
-    };
-    ModalWindow.prototype.initEvents = function () {
-        var _this = this;
-        var body = document.querySelector('body');
-        body.addEventListener('keydown', function (e) {
-            if (e.key == Keys.Esc) { // TODO: add to enum
-                _this.hide();
-            }
-        });
-        // TODO: check native events
-        this.modalClose.addEventListener('click', function (e) {
-            _this.hide();
-        });
-        this.modalWindow.addEventListener('click', function (e) {
-            if (!_this.modalDialog.contains(e.target)) {
-                _this.hide();
-            }
-        });
-    };
-    ModalWindow.prototype.show = function () {
+    }
+
+    public initEvents(): void {
+        let body = document.querySelector('body');
+            body.addEventListener('keydown', (e) => {
+                if (e.key == Keys.Esc) {
+                    this.hide()
+                }
+            });
+            // TODO: check native events
+            this.modalClose.addEventListener('click', (e) => {
+                this.hide();
+            });
+            this.modalWindow.addEventListener('click', (e) => {
+                if (!this.modalDialog.contains(e.target as Element)) {
+                    this.hide();
+                }
+
+            });
+    }
+    
+    public show(): void {
         if (this.showed == false) {
             this.modalWindow.style.opacity = '1';
             this.showed = true;
         }
-    };
-    ModalWindow.prototype.hide = function () {
+    }
+
+    public hide(): void {
         if (this.showed == true) {
             this.modalWindow.style.opacity = '0';
             this.modalWindow.style.pointerEvents = 'none';
             this.showed = false;
         }
-    };
-    ModalWindow.prototype.disableCloseButton = function () {
+    }
+
+    public disableCloseButton(): void {
         this.modalClose.style.pointerEvents = 'none';
         this.modalClose.style.opacity = '0';
-    };
-    ModalWindow.prototype.addContent = function (content) {
+    }
+
+    addContent(content: HTMLElement): void {
         this.modalBody.appendChild(content);
-    };
-    ModalWindow.prototype.changeTitle = function (str) {
+    }
+
+    changeTitle(str: string): void {
         this.modalTitle.innerText = str;
-    };
-    return ModalWindow;
-}());
-var ModalWindowInstance = ModalWindow.getInstance();
-export default ModalWindowInstance;
+    }
+}
+
+const ModalWindowInstance = ModalWindow.getInstance();
+
+export default ModalWindowInstance; 
